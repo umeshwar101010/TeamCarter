bb7eaf9c1c7535472ab49ef48ed46394d6505939
Miguel Costa <miguelbarca.usc@gmail.com>
2020-02-18 14:01:51 +0000

    more work. Controls for the player established

--- CatsCridle/src/Objects/Player.java
+++ CatsCridle/src/Objects/Player.java
@@ -1,6 +1,32 @@
 package Objects;
 
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
 public class Player extends GameObject{
     private String name;
+
+    private Picture playerObject;
+
+    public void spawn(){
+        playerObject = new Picture(20, 20,"Images/PlayerObj/playerobj.png");
+        playerObject.draw();
+    }
+
+    public void moveRight() {
+        playerObject.translate(10, 0);
+    }
+
+    public void moveLeft() {
+        playerObject.translate(-10, 0);
+    }
+
+    public void moveUp() {
+        playerObject.translate(0,-10);
+    }
+
+    public void moveDown() {
+        playerObject.translate(0,+10);
+    }
+
 
 }
