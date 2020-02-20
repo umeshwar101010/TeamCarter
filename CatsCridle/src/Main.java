e2697820c686796f38d3b4f74a3f6bc93227f12b
Francisco Vieira <fgrfv@iscte-iul.pt>
2020-02-20 08:47:53 +0000

    audio feature stable for playing one track

--- CatsCridle/src/Main.java
+++ CatsCridle/src/Main.java
@@ -1,12 +1,19 @@
 import Objects.Player;
 import com.github.TeamCarter.Frame.Canvas;
 import com.github.TeamCarter.Frame.Controls;
+import org.academiademiadecodigo.hackstreetboys.teamcarter.Audio;
+import org.academiademiadecodigo.hackstreetboys.teamcarter.Music;
 
 public class Main {
     public static void main(String[] args) {
 
         Canvas canvas= new Canvas(423, 850);
         canvas.makeCanvas();
+
+
+        //Music starts after canvas is made
+        Audio audio = new Audio();
+        audio.startAudio();
 
         Player player = new Player();
         player.spawn();
