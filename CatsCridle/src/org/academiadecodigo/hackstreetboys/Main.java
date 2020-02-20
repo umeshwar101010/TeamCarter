a736ba19ebf35c573508c1397623ca565f2a0940
Francisco Vieira <fgrfv@iscte-iul.pt>
2020-02-20 21:56:41 +0000

    fixed the main class

--- CatsCridle/src/org/academiadecodigo/hackstreetboys/Main.java
+++ CatsCridle/src/org/academiadecodigo/hackstreetboys/Main.java
@@ -4,8 +4,7 @@ import org.academiadecodigo.hackstreetboys.Engine.GameEngine;
 
 public class Main {
 
-    public static int PADDING=10;
-    public static int MOVEMENT=10;
+
     public static void main(String[] args) {
 
 
